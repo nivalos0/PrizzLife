8b6def3ed0a1f034f3779c4a450b4e8fa33e9869
nivalos0 <isaiahepperson80@gmail.com>
2026-02-08 17:10:44 -0500

    Update TeamGuiRemake.lua

--- Init/TeamGuiRemake.lua
+++ Init/TeamGuiRemake.lua
@@ -1,117 +1,128 @@
--- New Team GUI
+local TeamFrame, Teams = nil, nil
+local CoreGui = game:GetService("CoreGui")
+do
+	local TeamGui = Instance.new("ScreenGui")
+	TeamFrame = Instance.new("Frame")
+	local GuardBtn = Instance.new("TextButton")
+	local UICorner = Instance.new("UICorner")
+	local UICorner_2 = Instance.new("UICorner")
+	local InmateBtn = Instance.new("TextButton")
+	local UICorner_3 = Instance.new("UICorner")
+	local CrimBtn = Instance.new("TextButton")
+	local UICorner_4 = Instance.new("UICorner")
+	local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
+	local CreditsFrame = Instance.new("Frame")
+	local Ellie = Instance.new("TextLabel")
+	local UICorner_5 = Instance.new("UICorner")
+	local PrizzText = Instance.new("TextLabel")
+	local UICorner_6 = Instance.new("UICorner")
+	local UIAspectRatioConstraint_2 = Instance.new("UIAspectRatioConstraint")
 
-local LMG2L = {};
+	TeamGui.Name = "TeamGui"
+	TeamGui.Parent = CoreGui:WaitForChild("PLADMIN_INITIALS", 69)
+	TeamGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-LMG2L["TeamGui_1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
-LMG2L["TeamGui_1"]["Name"] = [[TeamGui]];
-LMG2L["TeamGui_1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
+	TeamFrame.Name = "TeamFrame"
+	TeamFrame.Parent = TeamGui
+	TeamFrame.BorderSizePixel = 0
+	TeamFrame.BackgroundColor3 = Color3.fromRGB(55, 55, 55)
+	TeamFrame.Size = UDim2.new(0.57228, 0, 0.24899, 0)
+	TeamFrame.Position = UDim2.new(0.21123, 0, 0.73565, 0)
 
+	GuardBtn.Name = "GuardBtn"
+	GuardBtn.Parent = TeamFrame
+	GuardBtn.BorderSizePixel = 0
+	GuardBtn.TextSize = 18
+	GuardBtn.TextColor3 = Color3.fromRGB(0, 122, 255)
+	GuardBtn.BackgroundColor3 = Color3.fromRGB(0, 6, 103)
+	GuardBtn.Size = UDim2.new(0.30901, 0, 0.81818, 0)
+	GuardBtn.Text = "GUARD"
+	GuardBtn.Position = UDim2.new(0.02146, 0, 0.09091, 0)
 
-LMG2L["TeamFrame_2"] = Instance.new("Frame", LMG2L["TeamGui_1"]);
-LMG2L["TeamFrame_2"]["BorderSizePixel"] = 0;
-LMG2L["TeamFrame_2"]["BackgroundColor3"] = Color3.fromRGB(55, 55, 55);
-LMG2L["TeamFrame_2"]["Size"] = UDim2.new(0.57228, 0, 0.24899, 0);
-LMG2L["TeamFrame_2"]["Position"] = UDim2.new(0.21123, 0, 0.73565, 0);
-LMG2L["TeamFrame_2"]["Name"] = [[TeamFrame]];
+	UICorner.Parent = GuardBtn
+	UICorner.CornerRadius = UDim.new(0, 20)
 
+	UICorner_2.Parent = TeamFrame
+	UICorner_2.CornerRadius = UDim.new(0, 15)
 
-LMG2L["GuardBtn_3"] = Instance.new("TextButton", LMG2L["TeamFrame_2"]);
-LMG2L["GuardBtn_3"]["BorderSizePixel"] = 0;
-LMG2L["GuardBtn_3"]["TextSize"] = 18;
-LMG2L["GuardBtn_3"]["TextColor3"] = Color3.fromRGB(0, 122, 255);
-LMG2L["GuardBtn_3"]["BackgroundColor3"] = Color3.fromRGB(0, 6, 103);
-LMG2L["GuardBtn_3"]["Size"] = UDim2.new(0.30901, 0, 0.81818, 0);
-LMG2L["GuardBtn_3"]["Text"] = [[GUARD]];
-LMG2L["GuardBtn_3"]["Name"] = [[GuardBtn]];
-LMG2L["GuardBtn_3"]["Position"] = UDim2.new(0.02146, 0, 0.09091, 0);
+	InmateBtn.Name = "InmateBtn"
+	InmateBtn.Parent = TeamFrame
+	InmateBtn.BorderSizePixel = 0
+	InmateBtn.TextSize = 18
+	InmateBtn.TextColor3 = Color3.fromRGB(255, 145, 0)
+	InmateBtn.BackgroundColor3 = Color3.fromRGB(182, 95, 16)
+	InmateBtn.Size = UDim2.new(0.30901, 0, 0.81818, 0)
+	InmateBtn.Text = "INMATE"
+	InmateBtn.Position = UDim2.new(0.34335, 0, 0.09091, 0)
 
+	UICorner_3.Parent = InmateBtn
+	UICorner_3.CornerRadius = UDim.new(0, 20)
 
-LMG2L["UICorner_4"] = Instance.new("UICorner", LMG2L["GuardBtn_3"]);
-LMG2L["UICorner_4"]["CornerRadius"] = UDim.new(0, 20);
+	CrimBtn.Name = "CrimBtn"
+	CrimBtn.Parent = TeamFrame
+	CrimBtn.BorderSizePixel = 0
+	CrimBtn.TextSize = 18
+	CrimBtn.TextColor3 = Color3.fromRGB(255, 0, 0)
+	CrimBtn.BackgroundColor3 = Color3.fromRGB(135, 22, 0)
+	CrimBtn.Size = UDim2.new(0.30901, 0, 0.81818, 0)
+	CrimBtn.Text = "CRIMINAL"
+	CrimBtn.Position = UDim2.new(0.66953, 0, 0.09091, 0)
 
+	UICorner_4.Parent = CrimBtn
+	UICorner_4.CornerRadius = UDim.new(0, 20)
 
-LMG2L["UICorner_5"] = Instance.new("UICorner", LMG2L["TeamFrame_2"]);
-LMG2L["UICorner_5"]["CornerRadius"] = UDim.new(0, 15);
+	UIAspectRatioConstraint.Parent = TeamFrame
+	UIAspectRatioConstraint.AspectRatio = 5.29545
 
+	CreditsFrame.Name = "CreditsFrame"
+	CreditsFrame.Parent = TeamGui
+	CreditsFrame.BorderSizePixel = 0
+	CreditsFrame.BackgroundColor3 = Color3.fromRGB(65, 65, 65)
+	CreditsFrame.Size = UDim2.new(0.52561, 0, 0.23201, 0)
+	CreditsFrame.Position = UDim2.new(0.23825, 0, -0.14713, 0)
 
-LMG2L["InmateBtn_6"] = Instance.new("TextButton", LMG2L["TeamFrame_2"]);
-LMG2L["InmateBtn_6"]["BorderSizePixel"] = 0;
-LMG2L["InmateBtn_6"]["TextSize"] = 18;
-LMG2L["InmateBtn_6"]["TextColor3"] = Color3.fromRGB(255, 145, 0);
-LMG2L["InmateBtn_6"]["BackgroundColor3"] = Color3.fromRGB(182, 95, 16);
-LMG2L["InmateBtn_6"]["Size"] = UDim2.new(0.30901, 0, 0.81818, 0);
-LMG2L["InmateBtn_6"]["Text"] = [[INMATE]];
-LMG2L["InmateBtn_6"]["Name"] = [[InmateBtn]];
-LMG2L["InmateBtn_6"]["Position"] = UDim2.new(0.34335, 0, 0.09091, 0);
+	Ellie.Name = "Ellie"
+	Ellie.Parent = CreditsFrame
+	Ellie.BorderSizePixel = 0
+	Ellie.TextSize = 18
+	Ellie.BackgroundColor3 = Color3.fromRGB(146, 146, 146)
+	Ellie.FontFace = Font.new("rbxasset://fonts/families/Arial.json", Enum.FontWeight.Regular, Enum.FontStyle.Normal)
+	Ellie.TextColor3 = Color3.fromRGB(255, 255, 255)
+	Ellie.BackgroundTransparency = 1
+	Ellie.Size = UDim2.new(0.97196, 0, 0.2439, 0)
+	Ellie.Text = "We love, and miss you Ellie!❤️🕊️"
+	Ellie.Position = UDim2.new(0.01168, 0, 0.03659, 0)
 
+	UICorner_5.Parent = CreditsFrame
 
-LMG2L["UICorner_7"] = Instance.new("UICorner", LMG2L["InmateBtn_6"]);
-LMG2L["UICorner_7"]["CornerRadius"] = UDim.new(0, 20);
+	PrizzText.Name = "PrizzText"
+	PrizzText.Parent = CreditsFrame
+	PrizzText.BorderSizePixel = 0
+	PrizzText.TextSize = 14
+	PrizzText.BackgroundColor3 = Color3.fromRGB(95, 95, 95)
+	PrizzText.FontFace = Font.new("rbxasset://fonts/families/Arial.json", Enum.FontWeight.Bold, Enum.FontStyle.Normal)
+	PrizzText.TextColor3 = Color3.fromRGB(255, 255, 255)
+	PrizzText.Size = UDim2.new(0.38785, 0, 0.58537, 0)
+	PrizzText.Text = "PrizzLife ReImagined"
+	PrizzText.Position = UDim2.new(0.29907, 0, 0.31707, 0)
 
+	UICorner_6.Parent = PrizzText
+	UICorner_6.CornerRadius = UDim.new(0, 10)
 
-LMG2L["CrimBtn_8"] = Instance.new("TextButton", LMG2L["TeamFrame_2"]);
-LMG2L["CrimBtn_8"]["BorderSizePixel"] = 0;
-LMG2L["CrimBtn_8"]["TextSize"] = 18;
-LMG2L["CrimBtn_8"]["TextColor3"] = Color3.fromRGB(255, 0, 0);
-LMG2L["CrimBtn_8"]["BackgroundColor3"] = Color3.fromRGB(135, 22, 0);
-LMG2L["CrimBtn_8"]["Size"] = UDim2.new(0.30901, 0, 0.81818, 0);
-LMG2L["CrimBtn_8"]["Text"] = [[CRIMINAL]];
-LMG2L["CrimBtn_8"]["Name"] = [[CrimBtn]];
-LMG2L["CrimBtn_8"]["Position"] = UDim2.new(0.66953, 0, 0.09091, 0);
+	UIAspectRatioConstraint_2.Parent = CreditsFrame
+	UIAspectRatioConstraint_2.AspectRatio = 5.21951
 
+	Teams = Instance.new("Folder")
+	Teams.Name = "Teams"
+	Teams.Parent = TeamFrame
+	
+	GuardBtn.Parent = Teams
+	InmateBtn.Parent = Teams
+	CrimBtn.Parent = Teams
+	
+	GuardBtn.Parent = TeamFrame
+	InmateBtn.Parent = TeamFrame
+	CrimBtn.Parent = TeamFrame
+end
 
-LMG2L["UICorner_9"] = Instance.new("UICorner", LMG2L["CrimBtn_8"]);
-LMG2L["UICorner_9"]["CornerRadius"] = UDim.new(0, 20);
-
-
-LMG2L["UIAspectRatioConstraint_a"] = Instance.new("UIAspectRatioConstraint", LMG2L["TeamFrame_2"]);
-LMG2L["UIAspectRatioConstraint_a"]["AspectRatio"] = 5.29545;
-
-
-LMG2L["CreditsFrame_b"] = Instance.new("Frame", LMG2L["TeamGui_1"]);
-LMG2L["CreditsFrame_b"]["BorderSizePixel"] = 0;
-LMG2L["CreditsFrame_b"]["BackgroundColor3"] = Color3.fromRGB(65, 65, 65);
-LMG2L["CreditsFrame_b"]["Size"] = UDim2.new(0.52561, 0, 0.23201, 0);
-LMG2L["CreditsFrame_b"]["Position"] = UDim2.new(0.23825, 0, -0.14713, 0);
-LMG2L["CreditsFrame_b"]["Name"] = [[CreditsFrame]];
-
-
-LMG2L["Ellie_c"] = Instance.new("TextLabel", LMG2L["CreditsFrame_b"]);
-LMG2L["Ellie_c"]["BorderSizePixel"] = 0;
-LMG2L["Ellie_c"]["TextSize"] = 18;
-LMG2L["Ellie_c"]["BackgroundColor3"] = Color3.fromRGB(146, 146, 146);
-LMG2L["Ellie_c"]["FontFace"] = Font.new([[rbxasset://fonts/families/Arial.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
-LMG2L["Ellie_c"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
-LMG2L["Ellie_c"]["BackgroundTransparency"] = 1;
-LMG2L["Ellie_c"]["Size"] = UDim2.new(0.97196, 0, 0.2439, 0);
-LMG2L["Ellie_c"]["Text"] = [[We love, and miss you Ellie!❤️🕊️]];
-LMG2L["Ellie_c"]["Name"] = [[Ellie]];
-LMG2L["Ellie_c"]["Position"] = UDim2.new(0.01168, 0, 0.03659, 0);
-
-
-LMG2L["UICorner_d"] = Instance.new("UICorner", LMG2L["CreditsFrame_b"]);
-
-
-
-LMG2L["PrizzText_e"] = Instance.new("TextLabel", LMG2L["CreditsFrame_b"]);
-LMG2L["PrizzText_e"]["BorderSizePixel"] = 0;
-LMG2L["PrizzText_e"]["TextSize"] = 14;
-LMG2L["PrizzText_e"]["BackgroundColor3"] = Color3.fromRGB(95, 95, 95);
-LMG2L["PrizzText_e"]["FontFace"] = Font.new([[rbxasset://fonts/families/Arial.json]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
-LMG2L["PrizzText_e"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
-LMG2L["PrizzText_e"]["Size"] = UDim2.new(0.38785, 0, 0.58537, 0);
-LMG2L["PrizzText_e"]["Text"] = [[PrizzLife ReImagined]];
-LMG2L["PrizzText_e"]["Name"] = [[PrizzText]];
-LMG2L["PrizzText_e"]["Position"] = UDim2.new(0.29907, 0, 0.31707, 0);
-
-
-LMG2L["UICorner_f"] = Instance.new("UICorner", LMG2L["PrizzText_e"]);
-LMG2L["UICorner_f"]["CornerRadius"] = UDim.new(0, 10);
-
-
-LMG2L["UIAspectRatioConstraint_10"] = Instance.new("UIAspectRatioConstraint", LMG2L["CreditsFrame_b"]);
-LMG2L["UIAspectRatioConstraint_10"]["AspectRatio"] = 5.21951;
-
-
-
-return LMG2L["TeamGui_1"], require;
+return TeamFrame, Teams
