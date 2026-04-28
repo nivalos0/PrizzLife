b535be1640c803d870e806d1d62b9abe3dbaf0f5
nivalos0 <isaiahepperson80@gmail.com>
2026-04-27 21:56:13 -0400

    Update TeamChange.lua

--- Tests/TeamChange.lua
+++ Tests/TeamChange.lua
@@ -1,10 +1,13 @@
+-- Variables
 local TeamEvent = game:GetService("ReplicatedStorage").Remotes.RequestTeamChange
 local plrs = game:GetService("Players")
 local plr = plrs.LocalPlayer
 local teams = game:GetService("Teams")
 
+-- TeamChange function
 local function TeamChange(teamname)
-  TeamEvent:InvokeServer(teams:FindFirstChild(teamname))
+  TeamEvent:InvokeServer(teams[teamname], 1)
 end
 
+-- Do TeamChange
 TeamChange("Guards")
